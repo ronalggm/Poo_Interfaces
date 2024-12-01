4e71d255c0b23da87679eab9f2857e9d6935f6d9
totone <ronalggm@gmail.com>
2024-12-01 20:47:38 -0300

    terminado metodo imprimir de curriculum

--- src/main/java/org/ronal/poointerfaces/imprenta/modelo/Curriculum.java
+++ src/main/java/org/ronal/poointerfaces/imprenta/modelo/Curriculum.java
@@ -1,12 +1,43 @@
 package org.ronal.poointerfaces.imprenta.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curriculum extends Hoja {
-   public Curriculum(String contenido){
-       super(contenido);
-   }
+
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+
+
+    public Curriculum(String contenido, String persona, String carrera) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>();
+    }
+
+    public Curriculum addExperiencia(String exp) {
+        experiencias.add(exp);
+        return this;
+    }
+
 
     @Override
     public String imprimir() {
-        return "";
+        StringBuilder sb = new StringBuilder("Nombre: ");
+        sb.append(persona).append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesion: ")
+                .append(this.carrera)
+                .append("\n")
+                .append("Experiencias: \n");
+        for (String exp : this.experiencias) {
+            sb.append("- ").append(exp).append("\n");
+        }
+
+        return sb.toString();
     }
+
+
 }
--- src/main/java/org/ronal/poointerfaces/imprenta/modelo/Hoja.java
+++ src/main/java/org/ronal/poointerfaces/imprenta/modelo/Hoja.java
@@ -3,6 +3,7 @@ package org.ronal.poointerfaces.imprenta.modelo;
 abstract public class Hoja {
     protected                                                                                                                                                                                    String contenido;
 
+
     public Hoja(String contenido) {
         this.contenido = contenido;
     }
