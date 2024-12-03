d69ce673e7e82b39f01faa43749ca8e6a5ccbd85
totone <ronalggm@gmail.com>
2024-12-02 21:13:10 -0300

    corregido gradle, deshabilitado avisos, probado metodos imprimir

--- src/main/java/org/ronal/poointerfaces/imprenta/EjemploImprenta.java
+++ src/main/java/org/ronal/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,27 @@
 package org.ronal.poointerfaces.imprenta;
 
+import org.ronal.poointerfaces.imprenta.modelo.*;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
 
+        Curriculum cv = new Curriculum("John doe", "Ingeniero de sistemas", "Resumen laboral...");
+        cv.addExperiencia("JAVA")
+                .addExperiencia("Oracle dba")
+                .addExperiencia("Spring Framework")
+                .addExperiencia("Desarrollador FullStack")
+                .addExperiencia("Angular");
+
+
+        Informe informe = new Informe("Martin Fower", "James", "Estudio sobre microservicios");
+
+        System.out.println();
+        imprimir(cv);
+        imprimir(informe);
+
+    }
+
+    public static void imprimir(Hoja imprimible) {
+        System.out.println(imprimible.imprimir());
     }
 }
